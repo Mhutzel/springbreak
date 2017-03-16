2957d3bec25ef67b606e4aa22154799c4af82d35
Mhutzel <maggh17@vt.edu>
2017-03-15 21:01:08 -0400

    image and rearrange topics

--- out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
@@ -1,7 +1,7 @@
 fil = new Array();
 fil["0"]= "concepts/c-intro.html@@@Spring Break 2017@@@Marjan is going home to Northern Virginia to spend spring break with her friends and family...";
-fil["1"]= "tasks/t-accomodation.html@@@How to sleep@@@Here we will learn about sleeping arrangements in NoVA...";
-fil["2"]= "tasks/t-destination.html@@@How to spend free time@@@Here we will learn what Marjan will do to enjoy her week off from school...";
-fil["3"]= "tasks/t-pay.html@@@How to pay@@@This task will outline how Marjan will pay for expenses during her break...";
-fil["4"]= "tasks/t-transportation.html@@@How to drive home@@@The steps below will outline how Marjan will pack her car and prepare for her trip home...";
-fil["5"]= "tasks/t-trouble.html@@@Troubleshooting@@@...";
+fil["1"]= "tasks/t-accomodation.html@@@Sleeping Arrangements@@@When Marjan arrives home she will settle into her childhood bedroom...";
+fil["2"]= "tasks/t-destination.html@@@Spending free time@@@Marjan will have time off from school to enjoy some free time...";
+fil["3"]= "tasks/t-pay.html@@@Paying for food and activities@@@Although her parents will provide home cooked meals and dinners out on occasion,\n        Marjan is still responsible for paying for any fun things she does with her friends like going to the movies or downtown...";
+fil["4"]= "tasks/t-transportation.html@@@Driving home@@@The drive to Northern Virginia from Blacksburg is 4 hours long so Marjan must prepare for her departure my packing her car and preparing for the drive...";
+fil["5"]= "tasks/t-trouble.html@@@Troubleshooting@@@With a week of fun with friends and family there are plenty of things that can go wrong like depleting a bank account, losing a wallet, or not finishing homework. No Money In Bank Account If Marjan...";
